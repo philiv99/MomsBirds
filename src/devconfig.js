14e6d81150a060494bafbd8c86f334ad3aae955a
Phil Hall <philiv99@gmail.com>
2020-12-05 17:25:25 -0500

    Update devconfig.js

--- src/devconfig.js
+++ src/devconfig.js
@@ -1,9 +1,9 @@
 
-var mapboxGLKey = 'xxxxxxxxxxxxxxxx';
-const witBirdInfoToken = 'xxxxxxxxxxxxxxx';
-const witBirdAppId = 'xxxxxxxxxxxxxxxx';
-const witCommandToken = 'xxxxxxxxxxxxxxxx';
-const witCommandAppId = 'xxxxxxxxxxxxxxx';
+var mapboxGLKey = 'xxxxxxxxxx';
+const witBirdInfoToken = 'xxxxxxxxxx';
+const witBirdAppId = 'xxxxxxxxxx';
+const witCommandToken = 'xxxxxxxxxx';
+const witCommandAppId = 'xxxxxxxxxx';
 const IGAPIURL =  'http://localhost:51202/api'
 
 export default { 
