d7a000092eb7c96bf52fdfa11c08325cd51517b3
Phil Hall <philiv99@gmail.com>
2020-12-05 17:24:58 -0500

    Update prodconfig.js

--- src/prodconfig.js
+++ src/prodconfig.js
@@ -1,10 +1,10 @@
 
-var mapboxGLKey = 'pk.eyJ1IjoicGhpbGl2IiwiYSI6ImNqdDV6NjQyOTBicGszeWxqZzl1MmIwbmwifQ.5XxqiL5_KUeJruTboUvZPg';
-const witBirdInfoToken = 'DO75OI6LGECDZQAAWYXDCSRCJ5CMYMWK';
-const witBirdAppId = '1128625314178787';
-const witCommandToken = 'LDYV57ARY5DZ2SN6P4GEP3PIEH3RPYIM';
-const witCommandAppId = '868703177271364';
-const IGAPIURL =  'http://api.infogoer.com/api'
+var mapboxGLKey = 'xxxxxxxxxx';
+const witBirdInfoToken = 'xxxxxxxxxx';
+const witBirdAppId = 'xxxxxxxxxx';
+const witCommandToken = 'xxxxxxxxxx';
+const witCommandAppId = 'xxxxxxxxxx';
+const IGAPIURL =  'xxxxxxxxxx'
 
 export default { 
     key: mapboxGLKey,
